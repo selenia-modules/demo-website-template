f257be3331d58add2a02c0980389ab0d641cd299
Mauro Mangas <mauro.mangas@impactwave.com>
2016-06-29 12:01:49 +0100

    New demo website template

--- src/Config/DemoWebsiteTemplateModule.php
+++ src/Config/DemoWebsiteTemplateModule.php
@@ -1,71 +1,107 @@
 <?php
-namespace SeleniaTemplates\DemoWebsite\Config;
+namespace SeleniaModules\DemoWebsiteTemplate\Config;
 
-use SeleniaTemplates\DemoWebsite\Controllers\Index;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Electro\Application;
 use Electro\Core\Assembly\Services\ModuleServices;
+use Electro\Interfaces\Http\RequestHandlerInterface;
 use Electro\Interfaces\Http\RouterInterface;
 use Electro\Interfaces\ModuleInterface;
 use Electro\Interfaces\Navigation\NavigationInterface;
 use Electro\Interfaces\Navigation\NavigationProviderInterface;
-use Selenia\Platform\Components\Base\PageComponent;
 
-class DemoWebsiteTemplateModule implements ModuleInterface, NavigationProviderInterface
+class DemoWebsiteTemplateModule implements
+  ModuleInterface
+  , RequestHandlerInterface       // remove this if this module will not provide routing
+  , NavigationProviderInterface   // remove this if this module will not provide navigation
 {
   /** @var RouterInterface */
   private $router;
+
+  /*
+   * Remove this method if this module will not provide routing.
+   * Also remove:
+   *   * the line with `RequestHandlerInterface` on the `implements` list above
+   *   * the `private $router` property
+   *   * the following items on method `configure()`:
+   *     * `RouterInterface $router` method parameter
+   *     * `$this->router = $router;`
+   *     * `->registerRouter ($this)`
+   *
+   * Either way, you should also remove this comment block when it's no longer needed.
+   */
 
   function __invoke (ServerRequestInterface $request, ResponseInterface $response, callable $next)
   {
     return $this->router
       ->set ([
-        // Example route implementing a self-contained component-like controller.
-        '.'     => Index::class,
-
-        // Example route using an automatic controller and an external view.
-        'page1' => factory (function (PageComponent $page) {
-          $page->templateUrl = 'page1.html'; // try changing this to page2 and see what happens...
-          return $page;
-        }),
-
-        // page2 is served via auto-routing (but only on debug/development mode)
+        '.'              => page ('index.html'),
+        'about'          => page ('about.html'),
+        'contact'        => page ('contact.html'),
+        'blog'           => page ('blog.html'),
+        'single-post'    => page ('single-post.html'),
+        'portfolio'      => page ('portfolio.html'),
+        'single-project' => page ('single-project.html'),
+        // '.' => Some::class,
       ])
       ->__invoke ($request, $response, $next);
   }
 
+  /*
+   * Remove, below, what you don't need; it will improve performance and it's easy to add it back later by using your
+   * IDE's auto-completion (ex. by pressing Ctrl+Space after a ->).
+   */
   function configure (ModuleServices $module, RouterInterface $router, Application $app)
   {
     $this->router = $router;
-    $app->name    = 'demo';            // session cookie name
-    $app->appName = 'Minimal Website'; // default page title; also displayed on title bar (optional)
-    $app->title   = '@ - Demo';        // @ = page title
+    $app->name    = 'electro';      // session cookie name
+    $app->appName = 'Electro Demo Website';     // default page title; also displayed on title bar (optional)
+    $app->title   = '@ - Electro Demo Website'; // @ = page title
     $module
-      ->publishPublicDirAs ('modules/demo-company/demo-project')
+      ->publishPublicDirAs ('modules/selenia-modules/demo-website-template')
       ->provideMacros ()
       ->provideViews ()
       ->registerRouter ($this)
       ->registerNavigation ($this);
   }
 
+  /*
+   * Remove this method if this module will not provide navigation.
+   * Also remove:
+   *   * the line with `NavigationProviderInterface` on the `implements` list above
+   *   * the following items on method `configure()`:
+   *     * `->provideNavigation ($this)`
+   *
+   * Either way, you should also remove this comment block when it's no longer needed.
+   */
   function defineNavigation (NavigationInterface $navigation)
   {
     $navigation->add ([
-      '' => $navigation
+      ''               => $navigation
         ->link ()
         ->id ('home')
-        ->title ('Home')
-        ->links ([
-          'page1' => $navigation
-            ->link ()
-            ->id ('page1')
-            ->title ('Page 1'),
-          'page2' => $navigation
-            ->link ()
-            ->id ('page2')
-            ->title ('Page 2'),
-        ]),
+        ->title ('Home'),
+      'about'          => $navigation
+        ->link ()
+        ->id ('about')
+        ->title ('About'),
+      'contact'        => $navigation
+        ->link ()
+        ->id ('contact')
+        ->title ('Contact'),
+      'single-post'    => $navigation
+        ->link ()
+        ->id ('single-post')
+        ->title ('Single Post'),
+      'porfolio'       => $navigation
+        ->link ()
+        ->id ('porfolio')
+        ->title ('Porfolio'),
+      'single-project' => $navigation
+        ->link ()
+        ->id ('single-project')
+        ->title ('Single project'),
     ]);
   }
 
